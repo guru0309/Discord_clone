d521a0feb48b28ea8643ea7d03a0aa6b34865702
ShivBaria <202001222@daiict.ac.in>
2024-05-03 02:05:34 +0530

    intial

---
+++ client/src/Download/home.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Container, Typography, Button, Grid, Box } from '@mui/material';
+
+function Home() {
+  return (
+    <Box
+      sx={{
+        backgroundImage: `url('8662428-epic-games-store.jpg')`,
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        minHeight: '100vh', // Ensure the box takes the full height of the viewport
+      }}
+    >
+      
+    </Box>
+  );
+}
+
+export default Home;
